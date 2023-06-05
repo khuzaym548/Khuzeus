a7d4bf2f67422bbb4635cdde598653a373611df7
Adnan Khuzaym <119403280+khuzaym548@users.noreply.github.com>
2023-06-06 01:15:54 +0530

    Add files via upload

---
+++ Reverse_a_string.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+#include<string>
+using namespace std; 
+int main() 
+{ 
+    string str = "Its me Khuzaym"; 
+    reverse(str.begin(), str.end()); 
+	cout<<"\n"<<str; 
+    return 0;
+}
